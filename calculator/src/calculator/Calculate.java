22981eaa1a44327129501cfd12bf032c96c1a0bc
lesykim <trchol01@naver.com>
2024-04-04 12:32:21 +0900

    Add run()

--- calculator/src/calculator/Calculate.java
+++ calculator/src/calculator/Calculate.java
@@ -1,19 +1,23 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Calculate {
-	public int add(int a, int b) {
+	private Scanner sc = new Scanner(System.in);
+	
+	private int add(int a, int b) {
 		return a + b;
 	}
-	public int subtract(int a, int b) {
+	
+	private int subtract(int a, int b) {
 		return a - b;
 	}
-	
 
-	public int multiply(int a, int b) {
+	private int multiply(int a, int b) {
 		return a * b;
 	}
 
-	public int divide(int a, int b) {
+	private int divide(int a, int b) {
 		if (b == 0) {
 			System.out.println("0으로 나눌순 없습니다.");
 			return 0;
@@ -21,5 +25,35 @@ public class Calculate {
 		return a / b;
 	}
 
-	
+	public void run() {
+		while(true) {
+			Input input = new Input();
+			
+			int a = input.getA();
+			int b = input.getB();
+			
+			String x = "";
+			while(true) {
+				System.out.print("+ - * / : ");
+				x = sc.next();
+				if(x.equals("+")||x.equals("-")||x.equals("*")||x.equals("/")) {
+					break;
+				}
+			}
+			
+			if(x.equals("+")) {
+				int c = add(a,b);
+				System.out.printf("%d %s %d = %d",a,x,b,c);
+			}else if(x.equals("-")) {
+				int c = subtract(a,b);
+				System.out.printf("%d %s %d = %d",a,x,b,c);
+			}else if(x.equals("*")) {
+				int c = multiply(a,b);
+				System.out.printf("%d %s %d = %d",a,x,b,c);
+			}else if(x.equals("/")) {
+				int c = divide(a,b);
+				System.out.printf("%d %s %d = %d",a,x,b,c);
+			}
+		}
+	}
 }
--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -3,7 +3,6 @@ package calculator;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("과연 바뀔까요?");
 		//merge from feature/calculate
 	}
 
